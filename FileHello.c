3c2b9d9fe9cfda9bd1b388469faa08134dda5968
anhle <lablueen@gmail.com>
2018-08-31 07:06:43 +0700

    test fseek()

--- FileHello.c
+++ FileHello.c
@@ -3,9 +3,15 @@
 int main(){
 
   FILE *fp;
+  long int pos;
   fp = fopen("file01.txt", "w+");
   fprintf(fp, "Hello, i was here!\n");
+  pos = ftell(fp);
+  printf("%ld\n", pos);
   fputs("hello hello...\n", fp);
+  fseek(fp, -4, SEEK_CUR);
+  pos = ftell(fp);
+  printf("%ld/n", pos);
   fclose(fp);
 
 }
