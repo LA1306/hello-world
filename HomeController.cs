be62bc3ec0f09e98137971796fa2802b873b52f3
LA1306 <40240489+LA1306@users.noreply.github.com>
2018-12-03 07:49:00 +0700

    hi

---
+++ HomeController.cs
@@ -0,0 +1,66 @@
+﻿using L5_Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L5_Data.Controllers
+{
+    public class HomeController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
+        public ActionResult Index(string searchTerm = null)
+        {
+            // var model = _db.Restaurants.ToList();
+
+            //var model = from r in _db.Restaurants
+            //            orderby r.Id descending
+            //            select new RestaurantListView
+            //            {
+            //                Name = r.Name,
+            //                City = r.City,
+            //                Country = r.Country
+            //            };
+
+            var model = _db.Restaurants
+                            //.OrderByDescending(r => r.Id)
+                            .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
+                            .Take(10)
+                            .Select(r => new RestaurantListView
+                                {
+                                    Name = r.Name,
+                                    City = r.City,
+                                    Country = r.Country
+                                }
+                            );
+
+            return View(model);
+            //return View(_db.Restaurants);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();      // clean ??
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ReviewController.cs
@@ -0,0 +1,52 @@
+﻿using L5_Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace L5_Data.Controllers
+{
+    public class ReviewController : Controller
+    {
+        OdeToFoodDb db = new OdeToFoodDb();
+
+        // GET: Review
+        public ActionResult Index(int id)
+        {
+            Restaurant restaurant = db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        public ActionResult Create(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reviews.Add(review);
+                db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantId });
+            }
+
+            return View(review);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
